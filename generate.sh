4b02a5f8eb71602c7244bb6c75a99aae2ebe38da
Paul den Otter <paul@fulltag.dev>
2024-04-08 15:27:57 +0200

    chore: simplify build pipeline

--- generate.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p sdk/src && \
-docker run --rm \
-  -v $PWD:/repo openapitools/openapi-generator-cli generate \
-  -i /repo/kinde-mgmt-api-specs.yaml \
-  -g typescript-fetch \
-  -o /repo/sdk/src && \
-pnpm -C sdk build
